7e4b1145748781d2170c75c974c25773d4d60254
AlfredoSolis2000 <alfredo.solissanchez@utxicotepec.edu.mx>
2023-09-25 09:19:39 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/movieapp_200291/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movieapp_200291
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_200291/home.dart';
+
+
+void main() {
+  runApp(new MaterialApp(
+    home: Home(),
+    //theme: new ThemeData.dark(),
+    debugShowCheckedModeBanner: false
+  ));
+}
